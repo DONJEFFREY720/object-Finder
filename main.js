2f09cc392942a42db51dab17b6b5674f289f0b23
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-08-04 19:05:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 function setup(){
      canvas = createCanvas(400,400)
      canvas.center()
-     
+     video = createCapture(VIDEO) ;
+     video.hide()
+}
+
+vdo =""
+model_status = ""
+objects = []
+
+function start(){
+     ObjD = ml5.objectDetector('cocossd',modelLoaded)
+     document.getElementById("status").innerHTML = "Status : DETECTING OBJECT"
 }
 
 function preload(){
      
+}
+
+function modelLoaded(){
+     console.log("model is ready")
+     model_status = true
+    
 }
